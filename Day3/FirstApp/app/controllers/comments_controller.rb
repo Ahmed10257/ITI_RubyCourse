6f9c88f9388300edd31466533d79b4167473c0fa
Ahmed Mansour <ahmed.mansour10257@gmail.com>
2024-05-15 16:35:02 +0300

    Day 3

---
+++ Day3/FirstApp/app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  def index
+    @article = Article.find(params[:article_id])
+    @comments = @article.comments
+  end
+
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
+end
