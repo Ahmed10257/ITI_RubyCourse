a39b9ce2b73d0b97b1fd0849f5652a1a26a5d8f6
Ahmed Mansour <ahmed.mansour10257@gmail.com>
2024-05-19 03:58:48 +0300

    updating Lab4

--- Day4/FirstApp/app/models/article.rb
+++ Day4/FirstApp/app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   include Visible
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
@@ -7,9 +8,6 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image
 
-  def self.public_count
-    where(visible: true).count
-  end
 
   def report
     self.report_count = (self.report_count || 0) + 1
